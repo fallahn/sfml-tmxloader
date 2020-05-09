64ae7441c2669c6359550c003f09cdbc314ab3e6
DmitruMoth <dimaretyunskih@gmail.com>
2020-05-09 19:36:26 +0300

    adapted for new version of box2d(commit: 1025f9a). Based on issue #49

--- include/tmx/tmx2box2d.hpp
+++ include/tmx/tmx2box2d.hpp
@@ -31,9 +31,9 @@ source distribution.
 
 #include <tmx/MapObject.hpp>
 
-#include <Box2D/Dynamics/b2World.h>
-#include <Box2D/Common/b2Math.h>
-#include <Box2D/Dynamics/b2Fixture.h>
+#include <box2d/b2_world.h>
+#include <box2d/b2_math.h>
+#include <box2d/b2_fixture.h>
 
 #include <vector>
 #include <queue>
